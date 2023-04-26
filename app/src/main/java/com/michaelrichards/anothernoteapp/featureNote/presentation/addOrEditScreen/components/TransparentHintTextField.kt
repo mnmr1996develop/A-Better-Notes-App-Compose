5d2f49a632a69119c9eda32fe6fb77f0dfa09829
mnmr1996develop <93550664+mnmr1996develop@users.noreply.github.com>
2023-04-26 14:27:27 -0400

    working on the note screen

---
+++ app/src/main/java/com/michaelrichards/anothernoteapp/featureNote/presentation/addOrEditScreen/components/TransparentHintTextField.kt
@@ -0,0 +1,40 @@
+package com.michaelrichards.anothernoteapp.featureNote.presentation.addOrEditScreen.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusState
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+
+
+
+@Composable
+fun TransparentHintTextField(
+    text: String,
+    hint: String,
+    modifier: Modifier = Modifier,
+    isHintVisible: Boolean = true,
+    textStyle: TextStyle = TextStyle(),
+    singleLine: Boolean = true,
+    onFocusChange: (FocusState) -> Unit,
+    onValueChane: (String) -> Unit
+) {
+    Box(modifier = modifier) {
+        BasicTextField(value = text,
+            onValueChange = onValueChane,
+            singleLine = singleLine,
+            textStyle = textStyle,
+            modifier = Modifier
+                .fillMaxWidth()
+                .onFocusChanged { onFocusChange(it) })
+
+        if (isHintVisible) {
+            Text(text = hint, style = textStyle, color = Color.DarkGray)
+        }
+    }
+}
